2a0eb851788d1aa53ef478d5248fddff1c734cbb
Shpend Bajgora <shpendbajgora91@gmail.com>
2020-12-30 04:53:42 +0100

    changes

--- src/app/common/models/rest-response.ts
+++ src/app/common/models/rest-response.ts
@@ -1,3 +1,3 @@
 export interface IRestResponse<T> {
-  data: T,
+  data: T;
 }
--- src/app/common/services/crud/crud.service.ts
+++ src/app/common/services/crud/crud.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpResponse } from '@angular/common/http';
 
 import { environment } from '../../../../environments/environment';
 import { appendQueryParams } from '../../utils/append-params';
 import { IRestResponse } from '../../models/rest-response';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +13,11 @@ export class CrudService<T = any> {
 
   private endpoint = environment.apiGateway;
 
-  constructor(private _http: HttpClient) {
+  constructor(private httpClient: HttpClient) {
   }
 
   get http(): HttpClient {
-    return this._http;
+    return this.httpClient;
   }
 
   public setEndpoint(endpoint: string): void {
@@ -31,35 +32,35 @@ export class CrudService<T = any> {
     return this.endpoint;
   }
 
-  public create(request: { path?: string, body: any }, options?: any) {
+  public create(request: { path?: string, body: any }, options?: any): Observable<HttpEvent<T>> {
 
     const endpoint = `${this.endpoint}${request.path || ''}`;
 
     return this.http.post<T>(endpoint, request.body, options);
   }
 
-  public read(request: { path: string, includes?: string[] }, options?: any) {
+  public read(request: { path: string, includes?: string[] }, options?: any): Observable<HttpEvent<IRestResponse<T>>> {
 
     const endpoint = appendQueryParams(`${this.endpoint}${request.path}`, request.includes);
 
     return this.http.get<IRestResponse<T>>(endpoint, options);
   }
 
-  public readAll(request: { path?: string, includes?: string[] } = { path: '' }, options?: any) {
+  public readAll(request: { path?: string, includes?: string[] } = { path: '' }, options?: any): Observable<HttpEvent<IRestResponse<T[]>>> {
 
     const endpoint = appendQueryParams(`${this.endpoint}${request.path}`, request.includes);
 
     return this.http.get<IRestResponse<T[]>>(endpoint, options);
   }
 
-  public update(request: { path: string, body: any }, options?: any) {
+  public update(request: { path: string, body: any }, options?: any): Observable<HttpEvent<T>> {
 
     const endpoint = `${this.endpoint}${request.path}`;
 
     return this.http.put<T>(endpoint, request.body, options);
   }
 
-  public delete(request: { path: string }, options?: any) {
+  public delete(request: { path: string }, options?: any): Observable<HttpEvent<T>> {
 
     const endpoint = `${this.endpoint}${request.path}`;
 
